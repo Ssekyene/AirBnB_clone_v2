34e43b797540cedf05c1ed3f13d148309c761679
Ssekyene Robert <robertssekyene05@gmail.com>
2024-08-15 12:26:03 +0000

    added example usage

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,6 +4,7 @@ script that starts a Flask web application
 Your web application must be listening on 0.0.0.0, port 5000
 Routes:/: display “Hello HBNB!”
 You must use the option strict_slashes=False in your route definition
+Example usage: AirBnB_clone_v2$ python3 -m web_flask.0-hello_route
 """
 
 from flask import Flask
